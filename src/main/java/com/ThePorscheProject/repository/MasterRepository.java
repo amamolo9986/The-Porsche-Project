570882d5cfeebc0655724d9a4dce6d1d9090e74d
amamolo9986 <amamolo9986@yahoo.com>
2024-08-03 19:24:38 -0500

    issue #43 resolved
    mostly. small bug found, but this is mostly complete. so i will close this and link the but in a new issue.
    issue #45

--- src/main/java/com/ThePorscheProject/repository/MasterRepository.java
+++ src/main/java/com/ThePorscheProject/repository/MasterRepository.java
@@ -1,9 +1,15 @@
 package com.ThePorscheProject.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ThePorscheProject.domain.Master;
 
 public interface MasterRepository extends JpaRepository<Master, Integer>{
+
+//	Master findBySeriesCategory(String seriesCategory);
+
+	List<Master> findBySeriesCategory(String seriesCategory);
 
 }
