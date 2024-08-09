f4cf6df6fccc1d2038458962e5ad305b1a836d63
amamolo9986 <amamolo9986@yahoo.com>
2024-08-09 17:53:11 -0500

    bug hit on issue53
    description in issue

---
+++ src/main/java/com/ThePorscheProject/service/RacingService.java
@@ -0,0 +1,28 @@
+package com.ThePorscheProject.service;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ThePorscheProject.domain.Master;
+import com.ThePorscheProject.repository.MasterRepository;
+
+@Service
+public class RacingService {
+	
+	private MasterRepository masterRepo;
+
+	public RacingService(MasterRepository masterRepo) {
+		super();
+		this.masterRepo = masterRepo;
+	}
+
+	public List<Master> findByLabel(String label) {
+		if("Racing Models".equals(label)) {
+			masterRepo.findByLabel(label);
+		}
+		return Collections.emptyList();
+	}
+
+}
